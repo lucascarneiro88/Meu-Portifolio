7bb50c0cbe68e5a074edd8ed96771e0b9987f730
Lucas <carneirolucas88@gmail.com>
2024-03-08 11:30:35 -0300

    Adiciona componente de erro para páginas não encontradas

---
+++ src/Pages/Error/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div>
+      <h1>404 - Página não encontrada</h1>
+      <p>A página que você está tentando acessar não existe.</p>
+    </div>
+  )
+}
+
+export default Error
