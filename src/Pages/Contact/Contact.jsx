f586b025ac03f6b395ef5b340f33c8bb8def3225
Lucas <carneirolucas88@gmail.com>
2024-03-08 16:00:04 -0300

    Adiciona informações de contato à página

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -4,6 +4,11 @@ function Contact() {
   return (
     <div>
       <h1>Meu contato</h1>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/lucas-carneiro-dos-santos-982a97268/">Meu Perfil</a></p>
+      <p>GitHub: <a href="https://github.com/lucascarneiro88">Meu Perfil do GitHub</a></p>
+      <p>Telefone: 48 991852137</p>
+      <p>E-mail: carneirolucas88@gmail.com</p>
+    
     </div>
   )
 }
