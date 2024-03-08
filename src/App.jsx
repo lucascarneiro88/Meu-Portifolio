51cdde3d812929ef3314d21125fdea2f9ae1de1a
Lucas <carneirolucas88@gmail.com>
2024-03-08 11:27:13 -0300

    Configura rotas no componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import HomePage from "./Pages/HomePage/HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home/Home.jsx";
+import Info from "./pages/Info/Info.jsx";
+import Contact from "./pages/Contact/Contact.jsx";
+import Projects from "./pages/Projects/Projects.jsx";
+import Error from "./pages/Error/Error.jsx";
 
 function App() {
   return (
-    <>
-    <div>
-    <HomePage />
-    </div>
-   
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/info" element={<Info/>}/>
+        <Route path="/Contact" element={<Contact/>}/>
+        <Route path="/Projects" element={<Projects/>}/>
+        <Route path="*" element={<Error/>}/>
+      </Routes>
+    </Router>
   );
 }
 
