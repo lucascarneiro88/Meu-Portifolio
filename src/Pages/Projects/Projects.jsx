29354c017ceba5c3fe94d349d6d9381258958045
Lucas <carneirolucas88@gmail.com>
2024-03-08 12:35:55 -0300

    Add links Projetos

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 function Projects() {
   return (
     <div>
       <h1>Meus projetos</h1>
+      <p>
+        Aqui está o link para o meu projeto na Vercel: <a href="https://projeto-lab-medical.vercel.app/">https://projeto-lab-medical.vercel.app/</a>
+      </p>
+      <p>
+        Aqui está o link para o meu outro projeto na Vercel: <a href="https://https-github-com-lucascarneiro88-lista-tarefas-react.vercel.app/">https://https-github-com-lucascarneiro88-lista-tarefas-react.vercel.app/</a>
+      </p>
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
