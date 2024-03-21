45a656915973b234874cd1e8e22b213fe4ef45ca
Lucas <carneirolucas88@gmail.com>
2024-03-21 16:37:14 -0300

    refactor: refatora component Projects

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
+import projectsData from '../../data/projectsData';
+import ProjectCard from '../../components/projectCardComponent/ProjectCard';
 
-function Projects() {
+const Projects = () => {
   return (
     <div>
-      <h1>Meus projetos</h1>
-      <p>
-        Aqui está o link para o meu projeto na Vercel: <a href="https://projeto-lab-medical.vercel.app/">https://projeto-lab-medical.vercel.app/</a>
-      </p>
-      <p>
-        Aqui está o link para o meu outro projeto na Vercel: <a href="https://https-github-com-lucascarneiro88-lista-tarefas-react.vercel.app/">https://https-github-com-lucascarneiro88-lista-tarefas-react.vercel.app/</a>
-      </p>
+      <h1>Meus Projetos</h1>
+      {projectsData.map(project => (
+        <ProjectCard key={project.id} project={project} />
+      ))}
     </div>
   );
 }
