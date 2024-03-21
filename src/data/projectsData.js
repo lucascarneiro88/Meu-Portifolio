1d2b78113b215350634f6115ec0dbb94ec1522f6
Lucas <carneirolucas88@gmail.com>
2024-03-21 16:16:37 -0300

    feat: renderiza components ProjectCard com dados de projetos

---
+++ src/data/projectsData.js
@@ -0,0 +1,32 @@
+const projectsData = [
+    {
+      id: 1,
+      title: "Projeto Lab Medical",
+      alt: "Imagem do projeto Lab Medical",
+      description: "Descrição do projeto Lab Medical...",
+      deployLink: "https://projeto-lab-medical.vercel.app/",
+      githubRepo: "https://github.com/seu-usuario/projeto-lab-medical",
+      image: "/caminho/para/imagem1.jpg" 
+    },
+    {
+      id: 2,
+      title: "Site Oficina Mecânica",
+      alt: "Imagem do projeto Site Oficina Mecânica ",
+      description: "Descrição do projeto Lista de Tarefas React...",
+      deployLink: "https://site-mauro-mecanico.vercel.app/",
+      githubRepo: "https://github.com/lucascarneiro88/Site_Oficina_Mecanica",
+      image: "/caminho/para/imagem2.jpg" 
+    },
+    {
+   
+        id: 3,
+        title: "Lista de Tarefas React",
+        alt: "Imagem do projeto Lista de Tarefas React",
+        description: "Descrição do projeto Lista de Tarefas React...",
+        deployLink: "https://https-github-com-lucascarneiro88-lista-tarefas-react-b5hujl2ia.vercel.app/",
+        githubRepo: "https://github.com/lucascarneiro88/Lista-tarefas-react",
+        image: "/caminho/para/imagem2.jpg" 
+      },
+  ];
+  
+  export default projectsData;
