3cb03e872f5c27e70dec859feb85cb864f84183b
Lucas <carneirolucas88@gmail.com>
2024-03-21 16:08:32 -0300

    feat: add component About

---
+++ src/Components/AboutComponent/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./about.css";
+
+function About() {
+  return (
+    <div className="about-paragrafo">
+      <p>
+        "A vida é uma jornada de aprendizado constante<br /> e resoluções de problemas.
+        <br />
+        Com uma abordagem tranquila <br />e uma atitude colaborativa,
+      estou   <br /> sempre pronto para contribuir<br /> com positividade em qualquer
+        equipe e projeto."
+      </p>
+    </div>
+  );
+}
+
+export default About;
