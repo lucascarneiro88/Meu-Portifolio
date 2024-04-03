1fe3b383b7b1670c6f2959edb2cc7d859cb2ebf0
Lucas <carneirolucas88@gmail.com>
2024-04-03 00:02:58 -0300

    refactor: inclui divs para estilização cria classe para style css

--- src/Components/projectCardComponent/ProjectCard.jsx
+++ src/Components/projectCardComponent/ProjectCard.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import './project-card.css';
+import projectsData from '../../data/projectsData';
 
-function ProjectCard({ project }) {
+function ProjectCard({ project }) { 
   return (
-    <div className="project-card">
-      <img src={project.image} alt={project.alt} /> 
-      <h2>{project.title}</h2>
-      <p>{project.description}</p>
-      <div className="buttons">
-        <button className="deploy-button">
-          <a href={project.deployLink} target="_blank" rel="noopener noreferrer">Deploy</a>
-        </button>
-        <button className="github-button">
-          <a href={project.githubRepo} target="_blank" rel="noopener noreferrer">GitHub</a>
-        </button>
+    <div className='project-card-container'>
+      <div className="image-container">
+        <img src={project.image} alt={project.alt} />
+      </div>
+      <div className='card-content'>
+        <h2>{project.title}</h2>
+        <p id='description'>{project.description}</p>
+        <div className="btn-container">
+          <button className="btn">
+            <a href={project.deployLink} target="_blank" rel="noopener noreferrer">Deploy</a>
+          </button>
+          <button className="btn">
+            <a href={project.githubRepo} target="_blank" rel="noopener noreferrer">GitHub</a>
+          </button>
+        </div>
       </div>
     </div>
   );
