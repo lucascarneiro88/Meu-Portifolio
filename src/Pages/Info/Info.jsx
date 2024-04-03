2aacfd2817dac6bb6b6121062db74e451109b9a6
Lucas <carneirolucas88@gmail.com>
2024-04-03 00:08:29 -0300

    refactor: extrai lógica de renderização para o component Info

--- src/Pages/Info/Info.jsx
+++ src/Pages/Info/Info.jsx
@@ -1,41 +1,13 @@
+// Info.jsx
 import React from "react";
+import InfoComponent from "../../components/InfoComponent/InfoComponent";
+import NavBar from "../../components/NavBarComponent/NavBar";
 
 function Info() {
   return (
     <div>
-      <section>
-        <h2>Foto</h2>
-        <img src="/caminho/para/minha-foto.jpg" alt="Minha Foto" />
-      </section>
-      <h1>Minhas Informações</h1>
-      <section>
-        <h2>Habilidades Técnicas</h2>
-        <ul>
-          <li>Tecnologias:</li>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>React.js</li>
-            <li>C#</li>
-            <li>.NET</li>
-            <li>SQL Server</li>
-            <li>GitHub</li>
-            <li>Docker</li>
-            <li>Cloud Computing</li>
-            <li>Clean Code</li>
-            <li>Figma</li>
-            <li>Metodologia Ágil</li>
-          </ul>
-        </ul>
-      </section>
-      <section>
-        <h2>Documentos</h2>
-        <ul>
-          <li><a href="/caminho/para/meu-curriculo.pdf" target="_blank" rel="noopener noreferrer">Currículo</a></li>
-          <li><a href="/caminho/para/meu-certificado.pdf" target="_blank" rel="noopener noreferrer">Certificado de Curso Fullstack</a></li>
-        </ul>
-      </section>
+      <NavBar pageTitle= "Minhas informações"/>
+      <InfoComponent/> 
     </div>
   );
 }
