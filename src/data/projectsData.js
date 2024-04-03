64c1bd655d1851f1fef7f51b0d12dac0bc626c3c
Lucas <carneirolucas88@gmail.com>
2024-04-03 00:03:51 -0300

    refactor: carrega imagens para ter base de estilização css

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -6,16 +6,16 @@ const projectsData = [
       description: "Descrição do projeto Lab Medical...",
       deployLink: "https://projeto-lab-medical.vercel.app/",
       githubRepo: "https://github.com/seu-usuario/projeto-lab-medical",
-      image: "/caminho/para/imagem1.jpg" 
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCnqaVJSPcZSAPYNSlDqFLwE4xMvvnL4w-0LHbwlRCVTHaKI2J_3jjCCVxqDJEyj7hlrY&usqp=CAU" 
     },
     {
       id: 2,
       title: "Site Oficina Mecânica",
       alt: "Imagem do projeto Site Oficina Mecânica ",
-      description: "Descrição do projeto Lista de Tarefas React...",
+      description: " elerisque turpis eu diam dapibus, ut varius odio ultricies. Sed auctor neque a dui pharetra, vitae sollicitudin lacus fermentum. Donec eget sapien ipsum. Vivamus vehicula metus quis leo maximus tincidunt.",
       deployLink: "https://site-mauro-mecanico.vercel.app/",
       githubRepo: "https://github.com/lucascarneiro88/Site_Oficina_Mecanica",
-      image: "/caminho/para/imagem2.jpg" 
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCnqaVJSPcZSAPYNSlDqFLwE4xMvvnL4w-0LHbwlRCVTHaKI2J_3jjCCVxqDJEyj7hlrY&usqp=CAU" 
     },
     {
    
@@ -25,7 +25,7 @@ const projectsData = [
         description: "Descrição do projeto Lista de Tarefas React...",
         deployLink: "https://https-github-com-lucascarneiro88-lista-tarefas-react-b5hujl2ia.vercel.app/",
         githubRepo: "https://github.com/lucascarneiro88/Lista-tarefas-react",
-        image: "/caminho/para/imagem2.jpg" 
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCnqaVJSPcZSAPYNSlDqFLwE4xMvvnL4w-0LHbwlRCVTHaKI2J_3jjCCVxqDJEyj7hlrY&usqp=CAU" 
       },
   ];
   
