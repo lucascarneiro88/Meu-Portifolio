fef6c64477674e4782ad370153b94cb70a26496f
Lucas <carneirolucas88@gmail.com>
2024-04-04 15:33:44 -0300

    feat: adiciona campo de stacks em projectsData

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,12 +1,19 @@
 const projectsData = [
     {
       id: 1,
-      title: "Projeto Lab Medical",
-      alt: "Imagem do projeto Lab Medical",
-      description: "Descrição do projeto Lab Medical...",
-      deployLink: "https://projeto-lab-medical.vercel.app/",
-      githubRepo: "https://github.com/seu-usuario/projeto-lab-medical",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCnqaVJSPcZSAPYNSlDqFLwE4xMvvnL4w-0LHbwlRCVTHaKI2J_3jjCCVxqDJEyj7hlrY&usqp=CAU" 
+    title: "Projeto Lab Medical",
+    alt: "Imagem do projeto Lab Medical",
+    description: "Descrição do projeto Lab Medical...",
+    deployLink: "https://projeto-lab-medical.vercel.app/",
+    githubRepo: "https://github.com/seu-usuario/projeto-lab-medical",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCnqaVJSPcZSAPYNSlDqFLwE4xMvvnL4w-0LHbwlRCVTHaKI2J_3jjCCVxqDJEyj7hlrY&usqp=CAU",
+    stacks: [
+      "/img/logo-stacks/logo-js.png",
+      "/img/logo-stacks/logo-react.png",
+      "/img/logo-stacks/logo-bootstrap.png",
+      "/img/logo-stacks/logo-css.png",
+     
+    ]
     },
     {
       id: 2,
@@ -15,7 +22,14 @@ const projectsData = [
       description: " elerisque turpis eu diam dapibus, ut varius odio ultricies. Sed auctor neque a dui pharetra, vitae sollicitudin lacus fermentum. Donec eget sapien ipsum. Vivamus vehicula metus quis leo maximus tincidunt.",
       deployLink: "https://site-mauro-mecanico.vercel.app/",
       githubRepo: "https://github.com/lucascarneiro88/Site_Oficina_Mecanica",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCnqaVJSPcZSAPYNSlDqFLwE4xMvvnL4w-0LHbwlRCVTHaKI2J_3jjCCVxqDJEyj7hlrY&usqp=CAU" 
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCnqaVJSPcZSAPYNSlDqFLwE4xMvvnL4w-0LHbwlRCVTHaKI2J_3jjCCVxqDJEyj7hlrY&usqp=CAU" ,
+      stacks: [
+        "/img/logo-stacks/logo-js.png",
+        "/img/logo-stacks/logo-react.png",
+        "/img/logo-stacks/logo-css.png",
+       
+       
+      ]
     },
     {
    
@@ -25,7 +39,13 @@ const projectsData = [
         description: "Descrição do projeto Lista de Tarefas React...",
         deployLink: "https://https-github-com-lucascarneiro88-lista-tarefas-react-b5hujl2ia.vercel.app/",
         githubRepo: "https://github.com/lucascarneiro88/Lista-tarefas-react",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCnqaVJSPcZSAPYNSlDqFLwE4xMvvnL4w-0LHbwlRCVTHaKI2J_3jjCCVxqDJEyj7hlrY&usqp=CAU" 
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCnqaVJSPcZSAPYNSlDqFLwE4xMvvnL4w-0LHbwlRCVTHaKI2J_3jjCCVxqDJEyj7hlrY&usqp=CAU" ,
+        stacks: [
+          "/img/logo-stacks/logo-js.png",
+          "/img/logo-stacks/logo-react.png",
+          "/img/logo-stacks/logo-css.png",
+         
+        ]
       },
   ];
   
