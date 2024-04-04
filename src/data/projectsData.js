8b84b22a0fa642f267be82c950f83293614b7d1e
Lucas <carneirolucas88@gmail.com>
2024-04-04 16:17:15 -0300

    feat : adiciona caminho para imagens de projetos

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -5,8 +5,8 @@ const projectsData = [
     alt: "Imagem do projeto Lab Medical",
     description: "Descrição do projeto Lab Medical...",
     deployLink: "https://projeto-lab-medical.vercel.app/",
-    githubRepo: "https://github.com/seu-usuario/projeto-lab-medical",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCnqaVJSPcZSAPYNSlDqFLwE4xMvvnL4w-0LHbwlRCVTHaKI2J_3jjCCVxqDJEyj7hlrY&usqp=CAU",
+    githubRepo: "https://github.com/lucascarneiro88/LABMedical-REACT",
+    image: "/img/img-projetos/lab-medical.png",
     stacks: [
       "/img/logo-stacks/logo-js.png",
       "/img/logo-stacks/logo-react.png",
@@ -22,7 +22,7 @@ const projectsData = [
       description: " elerisque turpis eu diam dapibus, ut varius odio ultricies. Sed auctor neque a dui pharetra, vitae sollicitudin lacus fermentum. Donec eget sapien ipsum. Vivamus vehicula metus quis leo maximus tincidunt.",
       deployLink: "https://site-mauro-mecanico.vercel.app/",
       githubRepo: "https://github.com/lucascarneiro88/Site_Oficina_Mecanica",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCnqaVJSPcZSAPYNSlDqFLwE4xMvvnL4w-0LHbwlRCVTHaKI2J_3jjCCVxqDJEyj7hlrY&usqp=CAU" ,
+      image: "/img/img-projetos/site-oficina.png" ,
       stacks: [
         "/img/logo-stacks/logo-js.png",
         "/img/logo-stacks/logo-react.png",
@@ -39,7 +39,7 @@ const projectsData = [
         description: "Descrição do projeto Lista de Tarefas React...",
         deployLink: "https://https-github-com-lucascarneiro88-lista-tarefas-react-b5hujl2ia.vercel.app/",
         githubRepo: "https://github.com/lucascarneiro88/Lista-tarefas-react",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCnqaVJSPcZSAPYNSlDqFLwE4xMvvnL4w-0LHbwlRCVTHaKI2J_3jjCCVxqDJEyj7hlrY&usqp=CAU" ,
+        image: "/img/img-projetos/lista-tarefas.png" ,
         stacks: [
           "/img/logo-stacks/logo-js.png",
           "/img/logo-stacks/logo-react.png",
