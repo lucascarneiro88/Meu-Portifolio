ceff6452e6089287197ed54be904d3853412cdfd
Lucas <carneirolucas88@gmail.com>
2024-04-04 16:17:44 -0300

    feat: cria link para retornar a pagina inicial

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Error() {
   return (
     <div>
       <h1>404 - Página não encontrada</h1>
       <p>A página que você está tentando acessar não existe.</p>
+      <Link to="/">Voltar para a página inicial</Link>
     </div>
-  )
+  );
 }
 
-export default Error
+export default Error;
