ef2a232f6546e21ea9f3116edaeff09df3c2d06e
Lucas <carneirolucas88@gmail.com>
2024-04-09 12:32:31 -0300

    feat: implementa ThemeProvider para gerenciamento de temas

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+import { ThemeProvider } from './context/ThemeContext.jsx';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+      <React.StrictMode>
+    <ThemeProvider >
+    <App/>
+    </ThemeProvider>
   </React.StrictMode>,
+ 
+
 )
