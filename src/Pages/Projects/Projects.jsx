5e8dc6fc05c91998682c89b23a3340d843d9cdba
Lucas <carneirolucas88@gmail.com>
2024-04-09 12:36:07 -0300

    style: ajustes de formatação no component Projects

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ProjectCard from '../../components/ProjectCardComponent/ProjectCard';
-import NavBar from '../../components/NavBarComponent/NavBar';
-import projectsData from '../../data/projectsData'; 
+import React from "react";
+import ProjectCard from "../../components/ProjectCardComponent/ProjectCard";
+import NavBar from "../../components/NavBarComponent/NavBar";
+import projectsData from "../../data/projectsData";
 
 function Projects() {
   return (
     <div>
-      <NavBar pageTitle="Meus Projetos" /> 
+      <NavBar pageTitle="Meus Projetos" />
       <div className="card-container">
-        {projectsData.map(project => ( 
-          <ProjectCard key={project.id} project={project} /> 
+        {projectsData.map((project) => (
+          <ProjectCard key={project.id} project={project} />
         ))}
       </div>
     </div>
