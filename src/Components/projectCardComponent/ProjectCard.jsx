a7c5c9999dfd8257d381287c938ece8bd5a878d5
Lucas <carneirolucas88@gmail.com>
2024-04-09 12:36:57 -0300

    style: ajusta layout do componente ProjectCard

--- src/Components/projectCardComponent/ProjectCard.jsx
+++ src/Components/projectCardComponent/ProjectCard.jsx
@@ -3,25 +3,27 @@ import './project-card.css';
 
 function ProjectCard({ project }) {
   return (
-    <div className='project-card-container'>
-      <div className="image-container">
-        <img src={project.image} alt={project.alt} />
-      </div>
-      <div className='card-content'>
-        <h2>{project.title}</h2>
-        <p id='description'>{project.description}</p>
-        <div className="stacks-container">
-          {project.stacks && Array.isArray(project.stacks) && project.stacks.map((stack, index) => (
-            <img key={index} src={stack} alt="Logo da tecnologia" />
-          ))}
+    <div className='page-container'>
+      <div className='project-card-container'>
+        <div className="image-container">
+          <img src={project.image} alt={project.alt} />
         </div>
-        <div className="btn-container">
-          <button className="btn">
-            <a href={project.deployLink} target="_blank" rel="noopener noreferrer">Deploy</a>
-          </button>
-          <button className="btn">
-            <a href={project.githubRepo} target="_blank" rel="noopener noreferrer">GitHub</a>
-          </button>
+        <div className='card-content'>
+          <h2>{project.title}</h2>
+          <p id='description'>{project.description}</p>
+          <div className="stacks-container">
+            {project.stacks && Array.isArray(project.stacks) && project.stacks.map((stack, index) => (
+              <img key={index} src={stack} alt="Logo da tecnologia" />
+            ))}
+          </div>
+          <div className="btn-container">
+            <button className="btn">
+              <a href={project.deployLink} target="_blank" rel="noopener noreferrer">Deploy</a>
+            </button>
+            <button className="btn">
+              <a href={project.githubRepo} target="_blank" rel="noopener noreferrer">GitHub</a>
+            </button>
+          </div>
         </div>
       </div>
     </div>
