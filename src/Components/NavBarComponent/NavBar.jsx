edbcbda4900069fd45c07347ef62d13fa1043ee8
Lucas <carneirolucas88@gmail.com>
2024-04-09 12:37:53 -0300

    feat: adiciona botão de alternar tema no NavBar

--- src/Components/NavBarComponent/NavBar.jsx
+++ src/Components/NavBarComponent/NavBar.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
 
 import "./navBar.css";
 
 function NavBar({ pageTitle }) {
+  const { toggleTheme } = useContext(ThemeContext);
   return (
     <nav className="navbar">
       <h1>{pageTitle}</h1>
+
+      <button onClick={toggleTheme}>Mudar Tema</button>
       <div className="links-container">
         <ul className="links-nav">
           <li>
