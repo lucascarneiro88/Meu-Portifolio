43e9e830747b9c9da87c723c7ef81a05a0908c69
Lucas <carneirolucas88@gmail.com>
2024-04-23 11:37:44 -0300

    feat: adiciona modal de detalhes ao component ProjectCard

--- src/Components/projectCardComponent/ProjectCard.jsx
+++ src/Components/projectCardComponent/ProjectCard.jsx
@@ -1,29 +1,65 @@
-import React from 'react';
-import './project-card.css';
+import React, { useState } from "react";
+import "./project-card.css";
+import ModalProject from "../ModalProjectComponent/ModalProject";
 
 function ProjectCard({ project }) {
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleModalOpen = () => {
+    setOpenModal(true);
+  };
+
+  const handleModalClose = () => {
+    setOpenModal(false);
+  };
+
   return (
-    <div className='page-container'>
-      <div className='project-card-container'>
+    <div className="page-container">
+      {openModal && (
+        <ModalProject
+          isOpen={openModal}
+          setModalOpen={handleModalClose}
+          project={project}
+          additionalInfo={project.additionalInfo}
+        ></ModalProject>
+      )}
+
+      <div className="project-card-container">
         <div className="image-container">
           <img src={project.image} alt={project.alt} />
         </div>
-        <div className='card-content'>
+        <div className="card-content">
           <h2>{project.title}</h2>
-          <p id='description'>{project.description}</p>
+          <p id="description">{project.description}</p>
           <div className="stacks-container">
-            {project.stacks && Array.isArray(project.stacks) && project.stacks.map((stack, index) => (
-              <img key={index} src={stack} alt="Logo da tecnologia" />
-            ))}
+            {project.stacks &&
+              Array.isArray(project.stacks) &&
+              project.stacks.map((stack, index) => (
+                <img key={index} src={stack} alt="Logo da tecnologia" />
+              ))}
           </div>
           <div className="btn-container">
             <button className="btn">
-              <a href={project.deployLink} target="_blank" rel="noopener noreferrer">Deploy</a>
+              <a
+                href={project.deployLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Deploy
+              </a>
             </button>
             <button className="btn">
-              <a href={project.githubRepo} target="_blank" rel="noopener noreferrer">GitHub</a>
+              <a
+                href={project.githubRepo}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
             </button>
           </div>
+
+          <button onClick={handleModalOpen}>Ver mais</button>
         </div>
       </div>
     </div>
